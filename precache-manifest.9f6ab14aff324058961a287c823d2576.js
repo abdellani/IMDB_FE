bc93e16ef266e0ad01fc484faa8270398b780751
abdellani <abdellani@gmail.com>
2019-11-24 16:37:14 +0100

    Updates

---
+++ precache-manifest.9f6ab14aff324058961a287c823d2576.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0379c1e47aeab4542c08feb13134d147",
+    "url": "/IMDB_FE/index.html"
+  },
+  {
+    "revision": "47ce4f4a1cd72b975551",
+    "url": "/IMDB_FE/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6532c62ace3d462d9a7f",
+    "url": "/IMDB_FE/static/css/main.060ecb10.chunk.css"
+  },
+  {
+    "revision": "47ce4f4a1cd72b975551",
+    "url": "/IMDB_FE/static/js/2.18a04e7a.chunk.js"
+  },
+  {
+    "revision": "6532c62ace3d462d9a7f",
+    "url": "/IMDB_FE/static/js/main.f53722da.chunk.js"
+  },
+  {
+    "revision": "409fe8014f8f3304f3da",
+    "url": "/IMDB_FE/static/js/runtime-main.c1bac627.js"
+  },
+  {
+    "revision": "7a6b4d4bebe20c5f80f5605125c7d0de",
+    "url": "/IMDB_FE/static/media/abominable.7a6b4d4b.png"
+  },
+  {
+    "revision": "51e776409a3cbf1934d8ab8b19b8a9b9",
+    "url": "/IMDB_FE/static/media/angelHasFallen.51e77640.jpg"
+  },
+  {
+    "revision": "7cb3b5e8e9342225011798a6849d6e18",
+    "url": "/IMDB_FE/static/media/angelHasFallen.7cb3b5e8.png"
+  },
+  {
+    "revision": "9fd968baf28878d28a1a1de396bd6947",
+    "url": "/IMDB_FE/static/media/downtonAbbey.9fd968ba.png"
+  },
+  {
+    "revision": "f1eeff88541d8ea070ce8764f5ee236a",
+    "url": "/IMDB_FE/static/media/got.f1eeff88.jpeg"
+  },
+  {
+    "revision": "7cbf82f4d8a7b6410b3379a447c1e8d3",
+    "url": "/IMDB_FE/static/media/greenbook.7cbf82f4.jpeg"
+  },
+  {
+    "revision": "ff9fb82dd92ce347c6285801c74ebdc8",
+    "url": "/IMDB_FE/static/media/hustlers.ff9fb82d.png"
+  },
+  {
+    "revision": "d474f2409f995bb86ee8adc8a60a6bc0",
+    "url": "/IMDB_FE/static/media/it-chapter.d474f240.webp"
+  },
+  {
+    "revision": "0042b941b9d0fd0ed621a150bad0bf09",
+    "url": "/IMDB_FE/static/media/terminator.0042b941.jpg"
+  },
+  {
+    "revision": "4e66665d781e3f44ddb4c3764dd7616d",
+    "url": "/IMDB_FE/static/media/theLionKing.4e66665d.jpeg"
+  },
+  {
+    "revision": "b2f9a96550f31e7baba5502f06367176",
+    "url": "/IMDB_FE/static/media/theLionKing.b2f9a965.png"
+  },
+  {
+    "revision": "c4ca3bd3379dfa122062f6f449415b15",
+    "url": "/IMDB_FE/static/media/twitter-logo.c4ca3bd3.png"
+  }
+]);
